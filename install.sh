4c711b1046c82182e339b0e7fdb1c7961a03fb21
myles <mylesbecker1509@gmail.com>
2019-12-05 16:51:46 -0700

    backgrounds fix

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 cp -r i3/ ~/.config/
 cp -r polybar/ ~/.config/
 cp -r scripts/ ~/.config/
-cp -r backgrounds/ /usr/share/backgrounds/
-cp -r backgrounds/ ~/.backgrounds/
+sudo cp -r backgrounds/* /usr/share/backgrounds/
+cp -r backgrounds/* ~/.backgrounds/
 cp -r rofi/ ~/.config/
