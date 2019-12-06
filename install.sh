16f4c71cd9366089272b5f14a0b266dcbe35639e
myles <mylesbecker1509@gmail.com>
2019-12-05 19:10:38 -0700

    bug

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -r i3/ ~/.config/
 cp -r polybar/ ~/.config/
+mkdir ~/.backgrounds
 cp -r scripts/ ~/.config/
 sudo cp -r backgrounds/* /usr/share/backgrounds/
 cp -r backgrounds/* ~/.backgrounds/
